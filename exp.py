b5f42e8dca01b68a38353ded01368d72f03ea265
dgab@gatech.edu <davgab@Davits-MacBook-Pro.local>
2022-04-15 04:27:33 -0400

    tr#2

--- exp.py
+++ exp.py
@@ -1,17 +1,24 @@
 import subprocess
 import re
+import sys
 #import pwntools
 
-proc = subprocess.Popen(['python', 'rocket.py', 'pe-Windows-x86-cmd'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+n = len(sys.argv)
+#print("Total arguments passed:", n)
+
+if n <= 1:
+	print("exiting...")
+	sys.exit()
+
+proc = subprocess.Popen(['python', 'rocket.py', sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 rocket_commands = "Z\nP\n"
 
 #output = proc.stdout.readline()
 #print(output)
 
-#out, _ = proc.communicate(bytes("Z\nP\n"))
+out, _ = proc.communicate(bytes("Z\nP\n"))
 #print(out.decode('utf-8'))
 
-proc.stdin.write(rocket_commands.encode())
-output = proc.stdout.read()
-print(output)
+#proc.stdin.write(rocket_commands.encode())
+#output = proc.stdout.read()

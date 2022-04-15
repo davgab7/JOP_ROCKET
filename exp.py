d961c5df9871186277a0069b6a94da3a58202980
dgab@gatech.edu <davgab@Davits-MacBook-Pro.local>
2022-04-15 04:34:37 -0400

    tr#2

--- exp.py
+++ exp.py
@@ -10,14 +10,25 @@ if n <= 1:
 	print("exiting...")
 	sys.exit()
 
-proc = subprocess.Popen(['python', 'rocket.py', sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+try:
+	proc = subprocess.Popen(['python', 'rocket.py', sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+except:
+	print("Check file name, exiting...")
+	sys.exit()
 
 rocket_commands = "Z\nP\n"
 
 #output = proc.stdout.readline()
 #print(output)
 
-out, _ = proc.communicate(bytes("Z\nP\n"))
+try:
+	out, _ = proc.communicate(bytes("Z\nP\n"))
+except:
+	print("Exiting...")
+	sys.exit()
+
+proc.terminate()
+sys.exit()
 #print(out.decode('utf-8'))
 
 #proc.stdin.write(rocket_commands.encode())
